af0b8f51261fac05ef93466a6cba972f2cbdceb5
szymon-s292 <stajszczykszymon@gmail.com>
2025-04-28 13:04:47 +0200

    Update opinions.tsx

--- app/ui/opinions.tsx
+++ app/ui/opinions.tsx
@@ -27,6 +27,9 @@ export default function Opinions() {
       id="scroll-container"
       className="flex lg:px-[15%] overflow-hidden lg:gap-12 gap-5 flex-nowrap text-center h-fit lg:justify-center overflow-x-auto items-center px-8 scroll-smooth"
     >
+
+
+
       <FadeInOnScroll delay={600}>
       <div className="transition-transform duration-300 hover:scale-105 bg-white p-4 h-fit lg:min-h-[500px] lg:min-w-[350px] lg:w-[350px] min-h-[400px] min-w-[250px] w-[250px] rounded-xl shadow-lg flex justify-evenly flex-col lg:p-10 items-center">
         <Image src={"/person.svg"} height={100} width={100} alt="person" className="rounded-full" />
@@ -39,7 +42,10 @@ export default function Opinions() {
       </div>
       </FadeInOnScroll>
 
-      <FadeInOnScroll delay={200}>
+
+
+
+   {/*   <FadeInOnScroll delay={200}>
         <div className=" transition-transform duration-300 hover:scale-105 flex flex-col lg:mb-24 py-15 lg:pb-10">
           <div className="bg-white p-4 h-fit lg:min-h-[530px] lg:min-w-[350px] lg:w-[380px] min-h-[430px] min-w-[250px] w-[250px] rounded-xl shadow-lg flex justify-evenly flex-col lg:p-10 items-center">
             <Image src={"/person.svg"} height={100} width={100} alt="person" className="rounded-full" />
@@ -51,19 +57,26 @@ export default function Opinions() {
             </p>
           </div>
         </div>
-      </FadeInOnScroll>
+      </FadeInOnScroll>*/}
+
+
+
+
 
       <FadeInOnScroll delay={400}>
         <div className="transition-transform duration-300 hover:scale-105 bg-white p-4 h-fit lg:min-h-[500px] lg:min-w-[350px] lg:w-[350px] min-h-[400px] min-w-[250px] w-[250px] rounded-xl shadow-lg flex justify-evenly flex-col lg:p-10 items-center">
           <Image src={"/person.svg"} height={100} width={100} alt="person" className="rounded-full" />
-          <p className="text-xl font-bold">Klient</p>
-          <StarRating stars={3} />
-          <p className="lg:text-lg lg:tracking-wide">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi dolorem molestias dolores unde vel, itaque,
-            porro at quasi ipsa sed mollitia commodi. Tempora praesentium nostrum quis? Assumenda, labore deserunt!
+          <p className="text-xl font-bold">Magdalena Konieczna</p>
+          <StarRating stars={5} />
+          <p className="lg:text-lg lg:tracking-wide" >Polecam współpracę z panią Ewą, staty kontakt telefoniczny, mailowy i na Whatsappie. Oprócz projektów pomagała mi
+także z dokumentacją na różnych etapach (warunki zabudowy, pozwolenie na budowę).
           </p>
         </div>
       </FadeInOnScroll>
+
+
+
+
     </div>
     </div>
 
